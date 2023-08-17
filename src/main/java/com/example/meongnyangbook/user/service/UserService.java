f6ab985131f213a5477e79fe7481595047ab06fe
SeungBum Baek <wer06099@naver.com>
2023-08-17 12:37:32 +0900

    Add(bsb): 핸드폰 인증 전송 기능 추가 #1

--- src/main/java/com/example/meongnyangbook/user/service/UserService.java
+++ src/main/java/com/example/meongnyangbook/user/service/UserService.java
@@ -2,12 +2,16 @@ package com.example.meongnyangbook.user.service;
 
 import com.example.meongnyangbook.common.ApiResponseDto;
 import com.example.meongnyangbook.user.dto.LoginRequestDto;
+import com.example.meongnyangbook.user.dto.PhoneRequestDto;
 import com.example.meongnyangbook.user.dto.SignupRequestDto;
 import jakarta.servlet.http.HttpServletResponse;
+import net.nurigo.java_sdk.exceptions.CoolsmsException;
 import org.springframework.http.ResponseEntity;
 
 public interface UserService {
     ResponseEntity<ApiResponseDto> signup(SignupRequestDto requestDto);
 
     ResponseEntity<ApiResponseDto> signin(LoginRequestDto loginRequestDto, HttpServletResponse response);
+
+    ResponseEntity<ApiResponseDto> sendMessage(PhoneRequestDto phoneRequestDto) throws CoolsmsException;
 }
--- src/main/java/com/example/meongnyangbook/user/service/UserServiceImpl.java
+++ src/main/java/com/example/meongnyangbook/user/service/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.meongnyangbook.user.service;
 
 import com.example.meongnyangbook.common.ApiResponseDto;
 import com.example.meongnyangbook.user.dto.LoginRequestDto;
+import com.example.meongnyangbook.user.dto.PhoneRequestDto;
 import com.example.meongnyangbook.user.dto.SignupRequestDto;
 import com.example.meongnyangbook.user.entity.User;
 import com.example.meongnyangbook.user.entity.UserRoleEnum;
@@ -10,13 +11,18 @@ import com.example.meongnyangbook.user.repository.UserRepository;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.nurigo.java_sdk.api.Message;
+import net.nurigo.java_sdk.exceptions.CoolsmsException;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.Optional;
+import java.util.Random;
 
 @Service
 @RequiredArgsConstructor
@@ -28,6 +34,15 @@ public class UserServiceImpl implements UserService{
     private final JwtUtil jwtUtil;
     @Value("${database.username}")
     private String ADMIN_TOKEN;
+
+    @Value("${coolsms.devHee.apikey}")
+    private String apiKey;
+
+    @Value("${coolsms.devHee.apisecret}")
+    private String apiSecret;
+
+    @Value("${coolsms.devHee.fromnumber}")
+    private String fromNumber;
 
     @Override
     public ResponseEntity<ApiResponseDto> signup(SignupRequestDto requestDto) {
@@ -91,6 +106,35 @@ public class UserServiceImpl implements UserService{
         response.addHeader(JwtUtil.AUTHORIZATION_HEADER, accessToken);
 
         return ResponseEntity.status(200).body(new ApiResponseDto("로그인 성공", HttpStatus.OK.value()));
+    }
+
+    @Override
+    public ResponseEntity<ApiResponseDto> sendMessage(PhoneRequestDto phoneRequestDto) throws CoolsmsException {
+        Random random = new Random();
+
+        // 0부터 9999 사이의 난수 생성
+        int randomNumber = random.nextInt(10000);
+
+        // 난수를 4자리 문자열로 변환 (앞에 0을 붙여줌)
+        String formattedRandomNumber = String.format("%04d", randomNumber);
+
+        Message coolsms = new Message(apiKey, apiSecret);
+
+            HashMap<String, String> params = new HashMap<String, String>();
+            params.put("to", phoneRequestDto.getPhoneNumber());
+            params.put("from", fromNumber);
+            params.put("type", "SMS");
+            params.put("text", "[grabMe] 인증번호 "+formattedRandomNumber+" 를 입력하세요.");
+            params.put("app_version", "test app 1.2"); // application name and version
+            try {
+                JSONObject obj = coolsms.send(params);
+                System.out.println(obj.toString());
+
+            } catch (CoolsmsException e) {
+                throw new CoolsmsException(e.getMessage(), e.getCode());
+            }
+        return ResponseEntity.status(200).body(new ApiResponseDto("핸드폰 인증번호 전송", HttpStatus.OK.value()));
+
     }
 
     private boolean checkAdmin(String adminToken) {
