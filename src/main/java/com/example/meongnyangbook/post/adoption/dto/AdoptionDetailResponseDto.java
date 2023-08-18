dd4a4fe9f944236e465c5828d00ded12f383b3d8
seulgi <asd0927122@gmail.com>
2023-08-18 14:56:06 +0900

    Add(ksg): QueryDsl 작성 - 미완성

--- src/main/java/com/example/meongnyangbook/post/adoption/dto/AdoptionDetailResponseDto.java
+++ src/main/java/com/example/meongnyangbook/post/adoption/dto/AdoptionDetailResponseDto.java
@@ -1,13 +1,20 @@
-package com.example.meongnyangbook.post.dto;
+package com.example.meongnyangbook.post.adoption.dto;
 
 import com.example.meongnyangbook.post.adoption.entity.Adoption;
+import com.example.meongnyangbook.post.dto.CommentResponseDto;
 import com.example.meongnyangbook.post.entity.AnimalGenderEnum;
 import com.example.meongnyangbook.post.entity.AreaEnum;
 import com.example.meongnyangbook.post.entity.CategoryEnum;
 import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
-public class AdoptionResponseDto {
+@Setter
+public class AdoptionDetailResponseDto {
     private String title;
     private String description;
     private Boolean completion;
@@ -16,8 +23,9 @@ public class AdoptionResponseDto {
     private Integer animalAge;
     private AreaEnum area;
     private CategoryEnum category;
+    private List<CommentResponseDto> commentList;
 
-    public AdoptionResponseDto(Adoption adoption) {
+    public AdoptionDetailResponseDto(Adoption adoption) {
         this.title = adoption.getTitle();
         this.description = adoption.getDescription();
         this.completion = adoption.getCompletion();
@@ -26,5 +34,9 @@ public class AdoptionResponseDto {
         this.animalAge = adoption.getAnimalAge();
         this.area = adoption.getArea();
         this.category = adoption.getCategory();
+        this.commentList = adoption.getCommentList()
+                .stream()
+                .map(CommentResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
