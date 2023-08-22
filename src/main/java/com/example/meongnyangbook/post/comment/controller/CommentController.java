480045c40151bc93456094dc8eb6b8d2a1b781c5
JungHoon <meslee70@gmail.com>
2023-08-22 17:34:09 +0900

    Add (KJH) : swagger 설정 추가 중 #3

--- src/main/java/com/example/meongnyangbook/post/comment/controller/CommentController.java
+++ src/main/java/com/example/meongnyangbook/post/comment/controller/CommentController.java
@@ -8,29 +8,38 @@ import com.example.meongnyangbook.user.details.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/mya/comments")
 public class CommentController {
 
-    private final CommentService commentService;
+  private final CommentService commentService;
 
-    @PostMapping
-    public CommentResponseDto createComment(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestBody CommentRequestDto commentRequestDto) {
-        return commentService.createComment(userDetails.getUser(), commentRequestDto);
-    }
+  @PostMapping
+  public CommentResponseDto createComment(@AuthenticationPrincipal UserDetailsImpl userDetails,
+      @RequestBody CommentRequestDto commentRequestDto) {
+    return commentService.createComment(userDetails.getUser(), commentRequestDto);
+  }
 
-    @PatchMapping("/{commentId}")
-    public CommentResponseDto updateComment(@PathVariable Long commentId, @AuthenticationPrincipal UserDetailsImpl userDetails, @RequestBody CommentRequestDto commentRequestDto) {
-        return commentService.updateComment(commentId, userDetails.getUser(), commentRequestDto);
-    }
+  @PatchMapping("/{commentId}")
+  public CommentResponseDto updateComment(@PathVariable Long commentId,
+      @AuthenticationPrincipal UserDetailsImpl userDetails,
+      @RequestBody CommentRequestDto commentRequestDto) {
+    return commentService.updateComment(commentId, userDetails.getUser(), commentRequestDto);
+  }
 
-    @DeleteMapping("/{commentId}")
-    public ResponseEntity<ApiResponseDto> deleteComment(@PathVariable Long commentId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        ApiResponseDto result = commentService.deleteComment(commentId, userDetails.getUser());
-        return ResponseEntity.ok().body(result);
-    }
-
+  @DeleteMapping("/{commentId}")
+  public ResponseEntity<ApiResponseDto> deleteComment(@PathVariable Long commentId,
+      @AuthenticationPrincipal UserDetailsImpl userDetails) {
+    ApiResponseDto result = commentService.deleteComment(commentId, userDetails.getUser());
+    return ResponseEntity.ok().body(result);
+  }
 }
--- src/main/java/com/example/meongnyangbook/post/like/service/LikeService.java
+++ src/main/java/com/example/meongnyangbook/post/like/service/LikeService.java
@@ -10,40 +10,41 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class LikeService {
-    private final LikeRepository likeRepository;
-    private final PostRepository postRepository;
+
+  private final LikeRepository likeRepository;
+  private final PostRepository postRepository;
 
 
-    public ResponseEntity<ApiResponseDto> createPostLike(Long postId, User user) {
-        Post post = postRepository.findById(postId).orElseThrow(() -> new IllegalArgumentException("해당 게시물이 없습니다."));
-        if(likeRepository.existsByPostIdAndUserId(postId,user.getId())){
-            //좋아요를 이미 누른 유저
+  @Transactional
+  public ResponseEntity<ApiResponseDto> createPostLike(Long postId, User user) {
+    Post post = postRepository.findById(postId)
+        .orElseThrow(() -> new IllegalArgumentException("해당 게시물이 없습니다."));
+    if (likeRepository.existsByPostIdAndUserId(postId, user.getId())) {
+      //좋아요를 이미 누른 유저
 
-            throw new IllegalArgumentException("이미 좋아요를 눌렀습니다.");
-        }
-        PostLike postLike = PostLike.builder().post(post).user(user).build();
-        likeRepository.save(postLike);
+      throw new IllegalArgumentException("이미 좋아요를 눌렀습니다.");
+    }
+    PostLike postLike = PostLike.builder().post(post).user(user).build();
+    likeRepository.save(postLike);
 
+    return ResponseEntity.status(200).body(new ApiResponseDto("좋아요", HttpStatus.OK.value()));
+  }
 
-        return ResponseEntity.status(200).body(new ApiResponseDto("좋아요", HttpStatus.OK.value()));
+  @Transactional
+  public ResponseEntity<ApiResponseDto> deletePostLike(Long postId, User user) {
+
+    if (likeRepository.existsByPostIdAndUserId(postId, user.getId())) {
+      PostLike postLike = likeRepository.findByPostIdAndUserId(postId, user.getId());
+      likeRepository.delete(postLike);
+    } else {
+      throw new IllegalArgumentException("해당 게시글 좋아요를 누르지 않음");
     }
 
-    public ResponseEntity<ApiResponseDto> deletePostLike(Long postId, User user) {
-
-        if(likeRepository.existsByPostIdAndUserId(postId,user.getId())) {
-            PostLike postLike = likeRepository.findByPostIdAndUserId(postId, user.getId());
-            likeRepository.delete(postLike);
-        }
-        else {
-            throw new  IllegalArgumentException("해당 게시글 좋아요를 누르지 않음");
-        }
-
-        return ResponseEntity.status(200).body(new ApiResponseDto("좋아요 취소", HttpStatus.OK.value()));
-    }
+    return ResponseEntity.status(200).body(new ApiResponseDto("좋아요 취소", HttpStatus.OK.value()));
+  }
 }
