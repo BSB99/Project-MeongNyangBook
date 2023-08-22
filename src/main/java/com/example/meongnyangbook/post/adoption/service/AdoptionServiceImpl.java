f18d6e81ae594bdd7ba0c8de520d187d9f6c9295
seulgi <asd0927122@gmail.com>
2023-08-22 10:11:57 +0900

    Remove(ksg): 은아님 작성한 파일 삭제

--- src/main/java/com/example/meongnyangbook/post/adoption/service/AdoptionServiceImpl.java
+++ src/main/java/com/example/meongnyangbook/post/adoption/service/AdoptionServiceImpl.java
@@ -2,12 +2,11 @@ package com.example.meongnyangbook.post.adoption.service;
 
 
 import com.example.meongnyangbook.common.ApiResponseDto;
-import com.example.meongnyangbook.post.adoption.entity.Adoption;
-import com.example.meongnyangbook.post.adoption.repository.AdoptionRepository;
 import com.example.meongnyangbook.post.adoption.dto.AdoptionDetailResponseDto;
 import com.example.meongnyangbook.post.adoption.dto.AdoptionReqeustDto;
 import com.example.meongnyangbook.post.adoption.dto.AdoptionResponseDto;
-import com.example.meongnyangbook.post.repository.PostRepository;
+import com.example.meongnyangbook.post.adoption.entity.Adoption;
+import com.example.meongnyangbook.post.adoption.repository.AdoptionRepository;
 import com.example.meongnyangbook.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -21,8 +20,6 @@ import java.util.stream.Collectors;
 public class AdoptionServiceImpl implements AdoptionService{
 
     private final AdoptionRepository adoptionRepository;
-    private final PostRepository postRepository;
-
     @Override
     public AdoptionResponseDto createAdoption(User user, AdoptionReqeustDto reqeustDto) {
         Adoption adoption = new Adoption(reqeustDto, user);
