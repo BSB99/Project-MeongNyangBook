c5fda70e297c59b1d52f285118adc2630f4f195c
SeungBum Baek <wer06099@naver.com>
2023-08-23 17:24:40 +0900

    Add(bsb): Shop CRUD 기능 작업 #40

--- src/main/java/com/example/meongnyangbook/aop/RoleCheckAop.java
+++ src/main/java/com/example/meongnyangbook/aop/RoleCheckAop.java
@@ -41,24 +41,6 @@ public class RoleCheckAop {
     @Pointcut("execution(* com.example.meongnyangbook.post.community.controller.CommunityController.deleteCommunity(..))")
     private void deleteCommunity() {}
 
-    @Pointcut("execution(* com.example.meongnyangbook.report.controller.ReportController.deleteReport(..))")
-    private void deleteReport() {}
-
-    @Pointcut("execution(* com.example.meongnyangbook.report.controller.ReportController.getReports(..))")
-    private void getReports() {}
-
-    @Pointcut("execution(* com.example.meongnyangbook.backoffice.notification.controller.NoticeController.createNotice(..))")
-    private void createNotice() {};
-
-    @Pointcut("execution(* com.example.meongnyangbook.backoffice.notification.controller.NoticeController.getNotices(..))")
-    private void getNotices() {};
-
-    @Pointcut("execution(* com.example.meongnyangbook.backoffice.notification.controller.NoticeController.updateNotice(..))")
-    private void updateNotice() {};
-
-    @Pointcut("execution(* com.example.meongnyangbook.backoffice.notification.controller.NoticeController.deleteNotice(..))")
-    private void deleteNotice() {};
-
     @Around("updateAdoption() || deleteAdoption()")
     public Object executePostAdoptionRoleCheck(ProceedingJoinPoint joinPoint) throws Throwable {
         // 1, 2번째 매개변수로 id, user값 가져오기
@@ -87,19 +69,6 @@ public class RoleCheckAop {
 
         if(!community.getUser().getUsername().equals(user.getUsername())) {
             throw new RejectedExecutionException("게시물 삭제 권한없습니다");
-        }
-
-        // 핵심 기능 수행
-        return joinPoint.proceed();
-    }
-
-    @Around("deleteReport() || getReports() || createNotice() || getNotices() || updateNotice() || deleteNotice()")
-    public Object executeReportRoleCheck(ProceedingJoinPoint joinPoint) throws Throwable {
-
-        UserDetailsImpl user = (UserDetailsImpl) joinPoint.getArgs()[0];
-
-        if (!user.getRole().equals(UserRoleEnum.ADMIN)) {
-            throw new RejectedExecutionException("신고 관련 권한이 없습니다");
         }
 
         // 핵심 기능 수행
