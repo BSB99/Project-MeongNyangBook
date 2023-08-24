e06616ab60f0fd8a02dcfabe385362efce23adec
SeungBum Baek <wer06099@naver.com>
2023-08-24 22:33:06 +0900

    Modified(bsb): Order_item queryDsl 기능 향상 #40

--- src/main/java/com/example/meongnyangbook/shop/basket/repository/BasketRepositoryQueryImpl.java
+++ src/main/java/com/example/meongnyangbook/shop/basket/repository/BasketRepositoryQueryImpl.java
@@ -2,19 +2,21 @@ package com.example.meongnyangbook.shop.basket.repository;
 
 import com.example.meongnyangbook.shop.basket.entity.Basket;
 import com.example.meongnyangbook.shop.order.entity.QOrderItem;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.querydsl.jpa.impl.JPAUpdateClause;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 public class BasketRepositoryQueryImpl implements BasketRepositoryQuery{
-    @PersistenceContext
-    private EntityManager em;
+    private final JPAQueryFactory jpaQueryFactory;
 
     @Override
     public void setBasketToNull(Long basketId) {
         QOrderItem orderItem = QOrderItem.orderItem;
 
-        new JPAUpdateClause(em, orderItem)
+        jpaQueryFactory.update(orderItem)
                 .set(orderItem.basket, (Basket) null)
                 .where(orderItem.basket.id.eq(basketId))
                 .execute();
