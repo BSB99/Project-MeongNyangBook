20e72b18e4cf24d5282cb61286fed314c5b32a54
SeungBum Baek <wer06099@naver.com>
2023-08-24 22:52:46 +0900

    Add(bsb): Swagger 설명 추가 #3

--- src/main/java/com/example/meongnyangbook/shop/order/controller/OrderController.java
+++ src/main/java/com/example/meongnyangbook/shop/order/controller/OrderController.java
@@ -5,20 +5,22 @@ import com.example.meongnyangbook.shop.order.dto.OrderRequestDto;
 import com.example.meongnyangbook.shop.order.dto.OrderResponseDto;
 import com.example.meongnyangbook.shop.order.service.OrderService;
 import com.example.meongnyangbook.user.details.UserDetailsImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
+@Tag(name = "주문 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/mya/orders")
 public class OrderController {
     private final OrderService orderService;
 
+    @Operation(summary = "주문 생성")
     @PostMapping
     public ResponseEntity<ApiResponseDto> createOrder(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestBody OrderRequestDto requestDto) {
         ApiResponseDto result = orderService.createOrder(userDetails.getUser(), requestDto);
@@ -26,6 +28,7 @@ public class OrderController {
         return ResponseEntity.status(HttpStatus.CREATED).body(result);
     }
 
+    @Operation(summary = "주문 상태 수정")
     @PatchMapping
     public ResponseEntity<ApiResponseDto> updateOrder(@AuthenticationPrincipal UserDetailsImpl userDetails) {
         ApiResponseDto result = orderService.updateOrder(userDetails.getUser());
@@ -33,6 +36,7 @@ public class OrderController {
         return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 
+    @Operation(summary = "주문완료된 항목 가져오기")
     @GetMapping
     public ResponseEntity<OrderResponseDto> getOrderList(@AuthenticationPrincipal UserDetailsImpl userDetails) {
         OrderResponseDto result = orderService.getOrderList(userDetails.getUser());
