2ebc2f690c14e0dd94e742aa8f8f3bb331f19853
SeungBum Baek <wer06099@naver.com>
2023-08-30 16:06:13 +0900

    Add(bsb): 결제시스템 연동 #40

--- src/main/resources/static/js/itemDetail.js
+++ src/main/resources/static/js/itemDetail.js
@@ -6,12 +6,17 @@ document.addEventListener("DOMContentLoaded", function() {
     let urlParts = window.location.href.split("/");
     itemNo = urlParts[urlParts.length - 1].replace("#", "");
 
-    const imgList = document.querySelectorAll(".col-md-3");
+    const imgList = document.querySelectorAll(".col-md-13");
     const imgDetailList = document.querySelectorAll(".col-md-9");
+    const itemInfoList = document.querySelectorAll(".col-lg-8");
+
     imgList.forEach((container) => {
         container.innerHTML = ``;
     })
     imgDetailList.forEach((container) => {
+        container.innerHTML = ``;
+    })
+    itemInfoList.forEach((container) => {
         container.innerHTML = ``;
     })
 
@@ -22,6 +27,11 @@ document.addEventListener("DOMContentLoaded", function() {
     let imgDetailFirstHtml = `<div class="tab-content">`;
     let imgDetailSecondHtml = ``;
     let imgDetailThirdHtml = ``;
+
+    let itemInfoFirstHtml = `<div class="product__details__text">`;
+    let itemInfoSecondHtml = ``;
+
+    let desc = "설명 없음";
     let no = 1;
     $.ajax({
         type: "GET",
@@ -32,22 +42,60 @@ document.addEventListener("DOMContentLoaded", function() {
     })
         .done((res) => {
             for (let fileUrls of res.fileUrls.fileName.split(",")) {
-                imgSecondHtml += `<li class="nav-item">
-                                <a class="nav-link active" data-toggle="tab" href="#tabs-${no}" role="tab">
-                                    <div class="product__thumb__pic set-bg" data-setbg="${fileUrls}">
+                if (no >= 2) {
+                    imgSecondHtml += `<li class="nav-item">
+                                <a class="nav-link" data-toggle="tab" href="#tabs-${no}" role="tab">
+                                    <div class="product__thumb__pic set-bg" data-setbg="${fileUrls}" style="background-image: url('${fileUrls}')">
                                     </div>
                                 </a>
                             </li>`
-                imgDetailSecondHtml += `
+                    imgDetailSecondHtml += `
+                <div class="tab-pane" id="tabs-${no}" role="tabpanel">
+                                <div class="product__details__pic__item">
+                                    <img src="${fileUrls}" alt="">
+                                </div>
+                            </div>
+                `
+                } else {
+                    imgSecondHtml += `<li class="nav-item">
+                                <a class="nav-link active" data-toggle="tab" href="#tabs-${no}" role="tab">
+                                    <div class="product__thumb__pic set-bg" data-setbg="${fileUrls}" style="background-image: url('${fileUrls}')">
+                                    </div>
+                                </a>
+                            </li>`
+                    imgDetailSecondHtml += `
                 <div class="tab-pane active" id="tabs-${no}" role="tabpanel">
                                 <div class="product__details__pic__item">
                                     <img src="${fileUrls}" alt="">
                                 </div>
                             </div>
                 `
+                }
                 no++;
             }
-
+            itemInfoSecondHtml += `
+            <h4>${res.name}</h4>
+                            <div class="rating">
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star-o"></i>
+                                <span> - 5 Reviews</span>
+                            </div>
+                            <h3>${res.price}원</h3>
+                            <p>남은 수량(${res.quantity})</p>
+                            <p>${res.content === undefined ? desc : res.content}</p>
+                            <div class="product__details__cart__option">
+                                <div class="quantity">
+                                    <div class="pro-qty">
+                                        <input type="text" value="1">
+                                    </div>
+                                </div>
+                                <a href="#" class="primary-btn">add to cart</a>
+                            </div>
+                        </div>
+            `;
             imgThirdHtml += `</ul>`;
             imgDetailThirdHtml += `</div>`
             imgList.forEach(container => {
@@ -56,6 +104,10 @@ document.addEventListener("DOMContentLoaded", function() {
 
             imgDetailList.forEach(container => {
                 container.innerHTML = imgDetailFirstHtml + imgDetailSecondHtml + imgDetailThirdHtml;
+            });
+
+            itemInfoList.forEach(container => {
+                container.innerHTML = itemInfoFirstHtml + itemInfoSecondHtml;
             });
         })
 })
