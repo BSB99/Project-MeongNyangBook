a960149843514855dc320b30200d430eb892e048
JungHoon <meslee70@gmail.com>
2023-09-05 10:57:12 +0900

    Refactor (KJH) : 변수 위치 지정 수정 #3

--- src/main/java/com/example/meongnyangbook/shop/basket/repository/BasketRepositoryQueryImpl.java
+++ src/main/java/com/example/meongnyangbook/shop/basket/repository/BasketRepositoryQueryImpl.java
@@ -1,21 +1,22 @@
 package com.example.meongnyangbook.shop.basket.repository;
 
 import com.example.meongnyangbook.shop.basket.Basket;
-import com.example.meongnyangbook.shop.order.entity.QOrderItem;
+import com.example.meongnyangbook.shop.order.QOrderItem;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-public class BasketRepositoryQueryImpl implements BasketRepositoryQuery{
-    private final JPAQueryFactory jpaQueryFactory;
+public class BasketRepositoryQueryImpl implements BasketRepositoryQuery {
 
-    @Override
-    public void setBasketToNull(Long basketId) {
-        QOrderItem orderItem = QOrderItem.orderItem;
+  private final JPAQueryFactory jpaQueryFactory;
 
-        jpaQueryFactory.update(orderItem)
-                .set(orderItem.basket, (Basket) null)
-                .where(orderItem.basket.id.eq(basketId))
-                .execute();
-    }
+  @Override
+  public void setBasketToNull(Long basketId) {
+    QOrderItem orderItem = QOrderItem.orderItem;
+
+    jpaQueryFactory.update(orderItem)
+        .set(orderItem.basket, (Basket) null)
+        .where(orderItem.basket.id.eq(basketId))
+        .execute();
+  }
 }
