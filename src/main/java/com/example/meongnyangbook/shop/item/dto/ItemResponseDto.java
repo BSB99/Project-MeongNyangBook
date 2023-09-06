964b396224263cc6ced3357f18e48d0af6a53c67
seulgi <asd0927122@gmail.com>
2023-09-06 20:29:37 +0900

    Refactor(ksg) : 수정 #3

--- src/main/java/com/example/meongnyangbook/shop/item/dto/ItemResponseDto.java
+++ src/main/java/com/example/meongnyangbook/shop/item/dto/ItemResponseDto.java
@@ -1,8 +1,8 @@
 package com.example.meongnyangbook.shop.item.dto;
 
+import com.example.meongnyangbook.S3.S3FileItemResponseDto;
 import com.example.meongnyangbook.shop.item.Item;
 import com.example.meongnyangbook.shop.item.ItemCategoryEnum;
-import com.example.meongnyangbook.shop.item.search.ItemDocument;
 import lombok.Getter;
 
 @Getter
@@ -14,7 +14,7 @@ public class ItemResponseDto {
   private final ItemCategoryEnum category;
   private final String createdAt;
   private final Long quantity;
-//  private final S3FileItemResponseDto fileUrls;
+  private final S3FileItemResponseDto fileUrls;
 
   public ItemResponseDto(Item item) {
     this.id = item.getId();
@@ -23,16 +23,6 @@ public class ItemResponseDto {
     this.category = item.getCategory();
     this.quantity = item.getQuantity();
     this.createdAt = item.getCreatedAtAsString();
-//    this.fileUrls = new S3FileItemResponseDto(item.getAttachmentItemUrl());
-  }
-
-  public ItemResponseDto(ItemDocument itemDocument) {
-    this.id = itemDocument.getItemId();
-    this.name = itemDocument.getName();
-    this.price = itemDocument.getPrice();
-    this.category = itemDocument.getCategory();
-    this.quantity = itemDocument.getQuantity();
-    this.createdAt = itemDocument.getCreatedAt().toString();
-//    this.fileUrls = new S3FileItemResponseDto(itemDocument.getAttachmentItemUrl());
+    this.fileUrls = new S3FileItemResponseDto(item.getAttachmentItemUrl());
   }
 }
