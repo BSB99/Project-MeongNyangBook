4a93354f50a580f0985214409432a939c3327017
JungHoon <meslee70@gmail.com>
2023-09-08 18:47:30 +0900

    Add (KJH) : Adoption 페이징처리 #1

--- src/main/resources/static/js/post-adoptions.js
+++ src/main/resources/static/js/post-adoptions.js
@@ -5,28 +5,106 @@ document.addEventListener("DOMContentLoaded", function () {
   // let userId = 1; // board 페이지에서 받아와야 하는 값
   const token = Cookies.get('Authorization');
 
-  $.ajax({
-    type: "GET",
-    url: "/mya/adoptions",
-    headers: {'Authorization': token}
-  })
-  .done(function (response) {
-    $('#adoptionPostList').empty();
-    for (let i = 0; i < response.length; i++) {
-      let adoptionTitle = response[i]['title']; //h5
-      let createdAt = response[i]['createdAt']; //span - 날짜
-      let imgUrl = response[i]['fileUrls']['fileName'].split(",")[0].split("/");
+  let page = 0;
+  let size = 9;
+  let postContainers = document.querySelectorAll("#adoptionPostList");
 
-      let adoptionId = response[i]['id']
-      console.log(adoptionId, adoptionTitle,
-          resizeS3FirstName + imgUrl[imgUrl.length - 1], createdAt);
-      setHtml(adoptionTitle, createdAt,
-          resizeS3FirstName + imgUrl[imgUrl.length - 1], adoptionId);
+  function goToPage(newPage) {
+    page = newPage; // 페이지 번호 업데이트
+    loadPostForPage(page); // 새 페이지 로드
+  }
+
+  function loadPostForPage(page) {
+
+    $.ajax({
+      type: "GET",
+      url: `/mya/adoptions?page=${page}&size=${size}`,
+    })
+    .done((response) => {
+      // Clear existing content before adding new content
+      postContainers.forEach(container => {
+        container.innerHTML = ''; // Clear existing content
+      });
+
+      let temp_html = '';
+
+      for (let i = 0; i < response.responseList.length; i++) {
+        const adoption = response.responseList[i];
+        //리사이징된 이미지 가져오는 코드
+        let adoptionfileName = adoption.fileUrls.fileName.split(",")[0].split(
+            "/");
+        let resizeItemName = resizeS3FirstName
+            + adoptionfileName[adoptionfileName.length - 1];
+
+        temp_html += `
+        <div class="col-lg-4 col-md-6 col-sm-6">
+        <div class="blog__item">
+          <div class="blog__item__pic set-bg" >
+          <img src="${resizeItemName}" alt="">
+          </div>
+          <div class="blog__item__text" value="${adoption.id}">
+            <span><img src="/img/icon/calendar.png" alt="">${adoption.createdAt}</span>
+            <h5>${adoption.title}</h5>
+            <a href="/mya/view/adoptions/${adoption.id}">Read More</a>
+          </div>
+        </div>
+      </div>
+      `;
+      }
+      console.log(response.len);
+      const rowHtml = `
+      <div class="row">${temp_html}</div>
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="product__pagination">
+            ${generatePaginationLinks(response.len / size + 1,
+          page)}
+          </div>
+        </div>
+      </div>`;
+
+      postContainers.forEach(container => {
+        container.innerHTML = rowHtml;
+      });
+    })
+    .fail((response) => {
+      console.log(response);
+    });
+  }
+
+  loadPostForPage(page);
+
+  document.addEventListener("click", function (event) {
+    if (event.target && event.target.matches(".product__pagination a")) {
+      event.preventDefault();
+      const newPage = parseInt(event.target.getAttribute("data-page"));
+      if (!isNaN(newPage)) {
+        goToPage(newPage);
+      }
     }
-  })
-  .fail(function (response) {
-    alert(response.responseJSON.msg);
-  })
+  });
+  // $.ajax({
+  //   type: "GET",
+  //   url: "/mya/adoptions",
+  //   headers: {'Authorization': token}
+  // })
+  // .done(function (response) {
+  //   $('#adoptionPostList').empty();
+  //   for (let i = 0; i < response.length; i++) {
+  //     let adoptionTitle = response[i]['title']; //h5
+  //     let createdAt = response[i]['createdAt']; //span - 날짜
+  //     let imgUrl = response[i]['fileUrls']['fileName'].split(",")[0].split("/");
+  //
+  //     let adoptionId = response[i]['id']
+  //     console.log(adoptionId, adoptionTitle,
+  //         resizeS3FirstName + imgUrl[imgUrl.length - 1], createdAt);
+  //     setHtml(adoptionTitle, createdAt,
+  //         resizeS3FirstName + imgUrl[imgUrl.length - 1], adoptionId);
+  //   }
+  // })
+  // .fail(function (response) {
+  //   alert(response.responseJSON.msg);
+  // })
 });
 
 function setHtml(adoptionTitle, createdAt, imgUrl, adoptionId) {
@@ -64,4 +142,20 @@ function start() {
       postBoxes[i].style.display = 'block';
     }
   }
+}
+
+function generatePaginationLinks(totalLen, currentPage) {
+  const totalPages = totalLen; // Total number of pages
+
+  let paginationLinks = '';
+  for (let i = 1; i <= totalPages; i++) {
+    if (i === currentPage + 1) {
+      paginationLinks += `<a class="active" href="#">${i}</a>`;
+    } else {
+      paginationLinks += `<a href="#" data-page="${i
+      - 1}">${i}</a>`;
+    }
+  }
+
+  return paginationLinks;
 }
