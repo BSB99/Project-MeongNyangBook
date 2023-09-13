78647754f5a0429698f0db681279e3d9b94a7be0
SeungBum Baek <wer06099@naver.com>
2023-09-13 16:14:17 +0900

    Add(bsb): 아이템 검색 필터 #40

--- src/main/resources/static/js/shop.js
+++ src/main/resources/static/js/shop.js
@@ -3,99 +3,26 @@ let category;
 let price;
 let page = 0;
 let paramUrl;
+
 document.addEventListener("DOMContentLoaded", function () {
   start();
-  let size = 6;
-  const resizeS3FirstName = "https://meongnyangs3.s3.ap-northeast-2.amazonaws.com/resize/";
-  const productContainers = document.querySelectorAll(".col-lg-9");
+
   category = handleCategoryClick();
   price = handlePriceRangeClick();
 
-  function loadItemsForPage(page, paramUrl) {
-    $.ajax({
-      type: "GET",
-      url: `/mya/items/es?page=${page}&size=${size}&${paramUrl}`,
-    })
-    .done((response) => {
-      // Clear existing content before adding new content
-      productContainers.forEach(container => {
-        container.innerHTML = ''; // Clear existing content
-      });
-
-      let itemsHtml = '';
-      for (let i = 0; i < response.itemList.length; i++) {
-        const item = response.itemList[i];
-        //리사이징된 이미지 가져오는 코드
-        let itemfileName = item.fileUrls.split(",")[0].split("/");
-        let resizeItemName = resizeS3FirstName
-            + itemfileName[itemfileName.length - 1];
-
-        itemsHtml += `
-        <div class="col-lg-4 col-md-6 col-sm-6">
-          <div class="product__item sale">
-            <div class="product__item__pic set-bg">
-              <a href="/mya/view/items/${item.id}">
-                <img src="${resizeItemName}" alt="" style="cursor: pointer;" onmouseover="this.style.cursor='pointer';" onmouseout="this.style.cursor='default';">
-              </a>
-              <ul class="product__hover">
-                <li><a href="#"><img src="/img/icon/heart.png" alt=""></a></li>
-                <li><a href="#"><img src="/img/icon/compare.png" alt=""> <span>Compare</span></a></li>
-                <li><a href="#"><img src="/img/icon/search.png" alt=""></a></li>
-              </ul>
-            </div>
-            <div class="product__item__text">
-              <h6>${item.name}</h6>
-              <a class="add-cart" data-itemno="${item.id}" href="#">+ Add To Cart</a>
-              <div class="rating">
-                <i class="fa fa-star"></i>
-                <i class="fa fa-star"></i>
-                <i class="fa fa-star"></i>
-                <i class="fa fa-star"></i>
-                <i class="fa fa-star-o"></i>
-              </div>
-              <h5>${item.price}원</h5>
-              <div class="product__color__select">
-                <label for="pc-7">
-                  <input type="radio" id="pc-7">
-                </label>
-                <label class="active black" for="pc-8">
-                  <input type="radio" id="pc-8">
-                </label>
-                <label class="grey" for="pc-9">
-                  <input type="radio" id="pc-9">
-                </label>
-              </div>
-            </div>
-          </div>
-        </div>
-      `;
-      }
-
-      const rowHtml = `
-      <div class="row">${itemsHtml}</div>
-      <div class="row">
-        <div class="col-lg-12">
-          <div class="product__pagination">
-            ${generatePaginationLinks(response.len / 6 + 1, page)}
-          </div>
-        </div>
-      </div>`;
-
-      productContainers.forEach(container => {
-        container.innerHTML = rowHtml;
-      });
-    })
-    .fail((response) => {
-      console.log(response);
-    });
-  }
+  loadItemsForPage(page, paramUrl)
 
   let min = "";
   let max = "";
 
   function handlePriceCategory(category, price) {
+    let itemName = document.getElementById("search-item").value;
     let url = "";
     let priceArray;
+
+    if(itemName.length > 0) {
+      url += `keyword=${itemName},`;
+    }
 
     if (price != null) {
       priceArray = price.split("-");
@@ -183,21 +110,6 @@ document.addEventListener("DOMContentLoaded", function () {
     return null;
   }
 
-  function generatePaginationLinks(totalLen, currentPage) {
-    const totalPages = totalLen; // Total number of pages
-
-    let paginationLinks = '';
-    for (let i = 1; i <= totalPages; i++) {
-      if (i === currentPage + 1) {
-        paginationLinks += `<a class="active" href="#">${i}</a>`;
-      } else {
-        paginationLinks += `<a href="#" data-page="${i - 1}">${i}</a>`;
-      }
-    }
-
-    return paginationLinks;
-  }
-
   $(document).on("click", ".product__pagination a", function (event) {
     event.preventDefault();
     const newPage = parseInt($(this).data("page"));
@@ -219,6 +131,102 @@ document.addEventListener("DOMContentLoaded", function () {
   loadItemsForPage(page, paramUrl);
 });
 
+function loadItemsForPage(page, paramUrl) {
+  let size = 6;
+  const resizeS3FirstName = "https://meongnyangs3.s3.ap-northeast-2.amazonaws.com/resize/";
+  const productContainers = document.querySelectorAll(".col-lg-9");
+
+  $.ajax({
+    type: "GET",
+    url: `/mya/items/es?page=${page}&size=${size}&${paramUrl}`,
+  })
+      .done((response) => {
+        // Clear existing content before adding new content
+        productContainers.forEach(container => {
+          container.innerHTML = ''; // Clear existing content
+        });
+
+        let itemsHtml = '';
+        for (let i = 0; i < response.itemList.length; i++) {
+          const item = response.itemList[i];
+          //리사이징된 이미지 가져오는 코드
+          let itemfileName = item.fileUrls.split(",")[0].split("/");
+          let resizeItemName = resizeS3FirstName
+              + itemfileName[itemfileName.length - 1];
+
+          itemsHtml += `
+        <div class="col-lg-4 col-md-6 col-sm-6">
+          <div class="product__item sale">
+            <div class="product__item__pic set-bg">
+              <a href="/mya/view/items/${item.id}">
+                <img src="${resizeItemName}" alt="" style="cursor: pointer;" onmouseover="this.style.cursor='pointer';" onmouseout="this.style.cursor='default';">
+              </a>
+              <ul class="product__hover">
+                <li><a href="#"><img src="/img/icon/heart.png" alt=""></a></li>
+                <li><a href="#"><img src="/img/icon/compare.png" alt=""> <span>Compare</span></a></li>
+                <li><a href="#"><img src="/img/icon/search.png" alt=""></a></li>
+              </ul>
+            </div>
+            <div class="product__item__text">
+              <h6>${item.name}</h6>
+              <a class="add-cart" data-itemno="${item.id}" href="#">+ Add To Cart</a>
+              <div class="rating">
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star-o"></i>
+              </div>
+              <h5>${item.price}원</h5>
+              <div class="product__color__select">
+                <label for="pc-7">
+                  <input type="radio" id="pc-7">
+                </label>
+                <label class="active black" for="pc-8">
+                  <input type="radio" id="pc-8">
+                </label>
+                <label class="grey" for="pc-9">
+                  <input type="radio" id="pc-9">
+                </label>
+              </div>
+            </div>
+          </div>
+        </div>
+      `;
+        }
+
+        const rowHtml = `
+      <div class="row">${itemsHtml}</div>
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="product__pagination">
+            ${generatePaginationLinks(response.len / 6 + 1, page)}
+          </div>
+        </div>
+      </div>`;
+
+        productContainers.forEach(container => {
+          container.innerHTML = rowHtml;
+        });
+      })
+      .fail((response) => {
+        console.log(response);
+      });
+}
+function generatePaginationLinks(totalLen, currentPage) {
+  const totalPages = totalLen; // Total number of pages
+
+  let paginationLinks = '';
+  for (let i = 1; i <= totalPages; i++) {
+    if (i === currentPage + 1) {
+      paginationLinks += `<a class="active" href="#">${i}</a>`;
+    } else {
+      paginationLinks += `<a href="#" data-page="${i - 1}">${i}</a>`;
+    }
+  }
+
+  return paginationLinks;
+}
 function addCart(itemNo) {
   const token = Cookies.get('Authorization');
 
@@ -243,6 +251,13 @@ function addCart(itemNo) {
       alert(response.msg);
     })
   }
+}
+
+function searchItem() {
+  let itemName = document.getElementById("search-item").value;
+  let url = "";
+  url = `keyword=${itemName}`
+  loadItemsForPage(page, url);
 }
 
 function start() {
